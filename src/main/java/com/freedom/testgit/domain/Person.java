3895e169c4515fc497ca15f3d8298aba0121555e
wujie <1787098791@qq.com>
2019-09-11 10:00:30 +0800

    提交分支

---
+++ src/main/java/com/freedom/testgit/domain/Person.java
@@ -0,0 +1,13 @@
+package com.freedom.testgit.domain;
+
+/**
+ * @author ：wujie
+ * @date ：Created in 2019/9/11 9:57
+ * @description：测试分支
+ * @version: 1.0.0
+ */
+public class Person {
+    private String name;
+
+    private Integer age;
+}
