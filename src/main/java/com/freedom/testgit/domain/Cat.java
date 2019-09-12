eaf452fbb07d7d3b73cc156802855c71649ef1e6
wujie <1787098791@qq.com>
2019-09-12 15:13:55 +0800

    提交

---
+++ src/main/java/com/freedom/testgit/domain/Cat.java
@@ -0,0 +1,12 @@
+package com.freedom.testgit.domain;
+
+/**
+ * @author ：wujie
+ * @date ：Created in 2019/9/12 15:13
+ * @description：
+ * @version:
+ */
+public class Cat {
+
+    private String name="1";
+}
