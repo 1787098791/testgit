b387ee6719d567fb17da3f9379bb8270f09f3d09
wujie <1787098791@qq.com>
2019-09-12 15:25:43 +0800

    提交

--- src/main/java/com/freedom/testgit/domain/Cat.java
+++ src/main/java/com/freedom/testgit/domain/Cat.java
@@ -2,20 +2,12 @@ package com.freedom.testgit.domain;
 
 /**
  * @author ：wujie
-<<<<<<< HEAD
- * @date ：Created in 2019/9/11 20:16
-=======
- * @date ：Created in 2019/9/12 15:13
->>>>>>> origin/dev-1
  * @description：
  * @version:
  */
 public class Cat {
 
-<<<<<<< HEAD
-    private String name="2";
-=======
-    private String name="1";
 
->>>>>>> origin/dev-1
+    private String name="2";
+
 }
