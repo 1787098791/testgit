3e3834324d8616b147593c85602f772e47123862
wujie <1787098791@qq.com>
2019-09-26 17:04:08 +0800

    2333

---
+++ src/main/java/com/freedom/testgit/domain/Dog.java
@@ -0,0 +1,12 @@
+package com.freedom.testgit.domain;
+
+/**
+ * @author ：wujie
+ * @date ：Created in 2019/9/11 20:06
+ * @description：测试分支
+ * @version: 1.0.0
+ */
+public class Dog {
+
+    private String name="旺旺222";
+}
