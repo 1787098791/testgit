f0ae1b8125773add13892a70ee2e23e973653438
wujie <1787098791@qq.com>
2020-06-12 13:33:10 +0800

    提交master

--- src/main/java/com/freedom/testgit/TestgitApplication.java
+++ src/main/java/com/freedom/testgit/TestgitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestgitApplication {
 
     public static void main(String[] args) {
-        System.out.println("233555");
+        System.out.println("233555888");
         SpringApplication.run(TestgitApplication.class, args);
     }
 
